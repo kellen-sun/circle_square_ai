9557eeb6a834bc4049dc2b0b5f3a5fb4e8ac8fc7
kellen-sun <ksun5@ocdsb.ca>
2022-03-09 20:32:14 -0500

    jjj

--- graphics.py
+++ graphics.py
@@ -1,1 +1,9 @@
-
+def g_console(array):
+    """Given an array it nicely prints and formats it on the console."""
+    for i in array:
+        for j in i:
+            if len(j)==2:
+                print(j, end="")
+            else:
+                print(j+" ", end="")
+        print("")
--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import random
 
+def generate_circle():
+    """Generates a random circle in a 32x32 grid. (marked with -1)"""
+    array=[[0]*32 for i in range(32)]
+    x=random.randint(10,22)
+    y=random.randint(10,22)
+    r=random.randint(6,10)
+    for i in range(len(array)):
+        for j in range(len(array)):
+            if (i-x)**2+(j-x)**2<=r**2:
+                array[i][j]=-1
+    for i in array:
+        for j in i:
+            if len(str(j))==2:
+                print(j, end="")
+            else:
+                print(str(j)+" ", end="")
+        print("")
+    return array
+generate_circle()
