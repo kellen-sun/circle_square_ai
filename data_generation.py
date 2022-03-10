e9af021e6afef6818bbc20cadba023ede5919c79
Computer23000 <alex.zimo.zh@gmail.com>
2022-03-09 20:42:12 -0500

    done data generation

--- data_generation.py
+++ data_generation.py
@@ -1,5 +1,5 @@
 import random
-def rectangle():
+def generate_rectangle():
     '''Generates a random circle in a 32 by 32 grid marked by 1'''
     # generates the 32 by 32 grid
     grid = [[0 for i in range(32)] for j in range(32)]
@@ -14,6 +14,28 @@ def rectangle():
             for j in range(32):
                 if j>=min(y1,y2) and j<=max(y1,y2):
                     grid[i][j] = 1
-    print(grid)
+    return grid
 
-rectangle()
+def generate_circle():
+    """Generates a random circle in a 32x32 grid. (marked with -1)"""
+    array=[[0]*32 for i in range(32)]
+    x=random.randint(10,22)
+    y=random.randint(10,22)
+    r=random.randint(6,10)
+    for i in range(len(array)):
+        for j in range(len(array)):
+            if (i-x)**2+(j-x)**2<=r**2:
+                array[i][j]=-1
+    '''for i in array:
+        for j in i:
+            if len(str(j))==2:
+                print(j, end="")
+            else:
+                print(str(j)+" ", end="")
+        print("")'''
+    return array
+
+for i in range(10000):
+    rectangle = generate_rectangle()
+    circle = generate_circle()
+print('done')
