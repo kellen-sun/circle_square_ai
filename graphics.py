c9066b2823ee37c8950179183dd5692d136e16b1
kellen-sun <ksun5@ocdsb.ca>
2022-03-09 20:32:51 -0500

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -2,8 +2,8 @@ def g_console(array):
     """Given an array it nicely prints and formats it on the console."""
     for i in array:
         for j in i:
-            if len(j)==2:
+            if len(str(j))==2:
                 print(j, end="")
             else:
-                print(j+" ", end="")
+                print(str(j)+" ", end="")
         print("")
