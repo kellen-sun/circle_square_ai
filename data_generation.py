bb4d87e855016887c04ca3ca196ab63e93b1d6b5
Computer23000 <alex.zimo.zh@gmail.com>
2022-03-10 19:13:32 -0500

    changed circle to string so we could write to file

--- data_generation.py
+++ data_generation.py
@@ -26,16 +26,27 @@ def generate_circle():
         for j in range(len(array)):
             if (i-x)**2+(j-x)**2<=r**2:
                 array[i][j]=-1
-    '''for i in array:
-        for j in i:
-            if len(str(j))==2:
-                print(j, end="")
-            else:
-                print(str(j)+" ", end="")
-        print("")'''
     return array
 
-for i in range(10000):
+def g_console(array):
+    """Given an array it nicely prints and formats it on the console."""
+    string = ''
+    for i in array:
+        for j in i:
+            if len(str(j))==2:
+                string = string+str(j)
+                #print(j, end="")
+            else:
+                string = string+str(j)+" "
+                #print(str(j)+" ", end="")
+        string =string+'\n'
+        #print("")
+    return string
+
+f = open("training_data.txt", "w")
+for i in range(10):
     rectangle = generate_rectangle()
     circle = generate_circle()
-print('done')
+    #f.write(g_console(generate_rectangle()))
+    f.write(g_console(circle))
+f.close()
